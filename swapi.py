16191f01b675fd262abf807af012619603594c6e
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-08-28 23:07:38 +0300

    11

---
+++ swapi.py
@@ -0,0 +1,13 @@
+import requests
+
+from pprint import pprint
+
+# response = requests.get('https://swapi.dev/api/people/')
+# res_lst = response.json()['results']
+
+# response = requests.get('https://swapi.dev/api/people/?search=Luke')
+# res_luke = response.json() 
+
+response = requests.get('https://swapi.dev/api/planets/1/')
+res_luke = response.json()['diameter'] 
+pass
