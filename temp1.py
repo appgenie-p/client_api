fd4a1325bd9cb612e35c279b740d5bf4c3e92204
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-09-08 10:27:41 +0300

    deleted garbage

--- temp1.py
+++
@@ -1,8 +0,0 @@
-class NegativeValueException(Exception):
-    pass
-
-def set_robot_power(value):
-    if value < 0:
-        raise NegativeValueException('Введите число, которое больше нуля!')
-
-set_robot_power(-1)
